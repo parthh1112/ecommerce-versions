eb9e52e65747c0ba4c935167646391c0e2b2fa72
parth gupta <parthgupta0824@gmail.com>
2025-02-21 16:48:10 +0530

    authentication

--- ecommerce 5/app.js
+++ ecommerce 5/app.js
@@ -4,14 +4,18 @@ const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
-
-
-
+const session = require('express-session')
+const flash = require('connect-flash')
+const passport = require('passport')
+const LocalStrategy = require('passport-local')
+const User = require("./models/user")
 
 
 mongoose.connect('mongodb://localhost:27017/shopping-app')
     .then(() => console.log('DB Connected'))
     .catch((err) => console.log(err));
+
+
 
 app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
@@ -19,42 +23,65 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
-
-
-
-
-
-
-const session = require('express-session')
-const flash = require('connect-flash')
-
-
-
 const sessionConfig = {
     secret: 'parthh1112',
     resave: false,
     saveUninitialized: true,
+    cookie:{
+        httpOnly:true,
+        expires:Date.now() + 1000*60*60*24, // 1 day
+        maxAge:1000*60*60*24
+    }
 }
-
 app.use(session(sessionConfig))
 app.use(flash())
+
+
+
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+
+
+
+// adding the user in session 
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser());
+
+
+
+
+passport.use(new LocalStrategy(User.authenticate()));// this authenticate function is add in our User model by passport-local-mongoose like it added username and password(hash)// this function will chk the username and password we provide in our database 
+
+
+
 
 
 app.use((req, res, next) => { // adding success and error globally now available to req and res & this success and error are automaticallly availablr to all the templates and we can use it like this - <%= success %> or  <%= error %> 
 
     res.locals.success = req.flash('success')
     res.locals.error = req.flash('error')
+
+    res.locals.currentUser = req.user
     next();
 })
 // Routes
 const productRoutes = require('./routes/product');
 const reviewRoutes = require('./routes/review');
+const authRoutes = require('./routes/auth');
 
 
 
 
 app.use(productRoutes);
 app.use(reviewRoutes);
+app.use(authRoutes);
+
+
+app.get('/', (req, res) => {
+    res.send('home page is empty')
+})
 
 
 const port = 4000;
